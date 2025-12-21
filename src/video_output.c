a495221c5665e46618d65a68a4ea735c6d9965f6
osemenyuk-114 <oleksandr.semenyuk@hotmail.com>
2025-12-21 21:43:59 +0100

    refactor: correct nosignal array dimensions for proper display formatting

--- src/video_output.c
+++ src/video_output.c
@@ -104,7 +104,7 @@ void draw_welcome_screen_h(video_mode_t video_mode)
   }
 }
 
-const char nosignal[14][114] = {
+const char nosignal[14][115] = {
     "xx      xx      xxxxxx                  xxxxxx      xxxxxx      xxxxxx      xx      xx        xx        xx",
     "xx      xx     xxxxxxxx                xxxxxxxx     xxxxxx     xxxxxxxx     xx      xx       xxxx       xx",
     "xxx     xx    xxx    xxx              xxx    xxx      xx      xxx    xxx    xxx     xx      xxxxxx      xx",
@@ -118,7 +118,8 @@ const char nosignal[14][114] = {
     "xx     xxx    xx      xx              xx      xx      xx      xx      xx    xx     xxx    xx      xx    xx",
     "xx     xxx    xxx    xxx              xxx    xxx      xx      xxx    xxx    xx     xxx    xx      xx    xx",
     "xx      xx     xxxxxxxx                xxxxxxxx     xxxxxx     xxxxxxxx     xx      xx    xx      xx    xxxxxxxxxx",
-    "xx      xx      xxxxxx                  xxxxxx      xxxxxx      xxxxxx      xx      xx    xx      xx    xxxxxxxxxx"};
+    "xx      xx      xxxxxx                  xxxxxx      xxxxxx      xxxxxx      xx      xx    xx      xx    xxxxxxxxxx",
+};
 
 void draw_no_signal(video_mode_t video_mode)
 {
